fb1928d4ce4c034577aa61fe3fb370b6f44e2f6c
Nick Lyman and Tommy Jones <nick@>
2016-04-26 16:49:37 -0700

    Final commit with click feed sleep and activity passing tests.

--- src/main/java/Tamagotchi.java
+++ src/main/java/Tamagotchi.java
@@ -1,6 +1,10 @@
 public class Tamagotchi {
+
   private String mName;
-  private Object mFood;
+  private int mFood = 10;
+  private int mSleep = 10;
+  private int mActivity = 10;
+  private int mTotalHealth;
 
   public Tamagotchi(String name) {
     mName = name;
@@ -8,10 +12,42 @@ public class Tamagotchi {
   public String getName() {
     return mName;
   }
-  public Tamagotchi(Object food) {
+  public Tamagotchi(int food, int sleep, int activity) {
     mFood = food;
+    mSleep = sleep;
+    mActivity = activity;
   }
-  public Object getFood() {
+  public int getFood() {
     return mFood;
   }
+  public int getSleep() {
+    return mSleep;
+  }
+  public int getActivity() {
+    return mActivity;
+  }
+  public int getTotalHealth() {
+    mTotalHealth = (mFood + mSleep + mActivity);
+    return mTotalHealth;
+  }
+  public int clickFeed(){
+    mFood++;
+    mSleep--;
+    mActivity--;
+    return mFood;
+  }
+  public int clickSleep(){
+    mFood--;
+    mSleep++;
+    mActivity--;
+    return mSleep;
+  }
+  public int clickActivity(){
+    mFood--;
+    mSleep--;
+    mActivity++;
+    return mActivity;
+  }
+
+
 }
--- src/test/java/TamagotchiTest.java
+++ src/test/java/TamagotchiTest.java
@@ -14,8 +14,20 @@ public class TamagotchiTest {
     assertEquals("lil dragon", myPet.getName());
   }
   @Test
-  public void tamagotch_getFoodLevel_Integer() {
-    Tamagotchi myPet = new Tamagotchi(10);
+  public void tamagotchi_getFoodLevel_int() {
+    Tamagotchi myPet = new Tamagotchi(10, 10, 10);
     assertEquals(10, myPet.getFood());
+  }
+  @Test
+  public void tamagotchi_totalHealth_int() {
+    Tamagotchi myPet = new Tamagotchi(10, 10 ,10);
+    assertEquals(30, myPet.getTotalHealth());
+  }
+  @Test
+  public void tamagotchi_clickFeed_int() {
+    Tamagotchi myPet = new Tamagotchi("lil dragon");
+    myPet.clickFeed();
+    assertEquals(9, myPet.getSleep());
+    assertEquals(9, myPet.getActivity());
   }
 }
